8758f2bfc53a2e414d4d0cb23c1baaca7ffc03a7
hengstchon <hengstchon@gmail.com>
2019-08-16 02:52:37 +0200

    拆分成不同js文件

--- ball.js
+++ ball.js
@@ -33,12 +33,3 @@ var Ball = function() {
 
   return o;
 };
-
-var rectIntersects = function(a, b) {
-  if (b.y > a.y && b.y < a.y + a.image.height) {
-    if (b.x > a.x && b.x < a.x + a.image.width) {
-      return true;
-    }
-  }
-  return false;
-};
