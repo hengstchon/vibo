ad67510262d887089a33a81f978524d1c63297fa
hengstchon <hengstchon@gmail.com>
2019-08-16 02:45:56 +0200

    拆分成不通js文件

---
+++ ball.js
@@ -0,0 +1,44 @@
+var Ball = function() {
+  var image = imageFromPath('ball.png');
+  var o = {
+    image: image,
+    x: 100,
+    y: 200,
+    speedX: 5,
+    speedY: 5,
+    fired: false,
+  };
+
+  o.fire = function() {
+    o.fired = true;
+  };
+
+  o.move = function() {
+    if (o.fired) {
+      if (o.x < 0 || o.x > 400) {
+        o.speedX = -o.speedX;
+      }
+      if (o.y < 0 || o.y > 300) {
+        o.speedY = -o.speedY;
+      }
+      // move
+      o.x += o.speedX;
+      o.y += o.speedY;
+    }
+  };
+
+  o.fantan = function() {
+    o.speedY *= -1;
+  };
+
+  return o;
+};
+
+var rectIntersects = function(a, b) {
+  if (b.y > a.y && b.y < a.y + a.image.height) {
+    if (b.x > a.x && b.x < a.x + a.image.width) {
+      return true;
+    }
+  }
+  return false;
+};
