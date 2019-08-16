57da33995497de44b943168fd325800949b9866f
hengstchon <hengstchon@gmail.com>
2019-08-16 04:50:47 +0200

    增加fps滑条

--- guagame.js
+++ guagame.js
@@ -31,7 +31,6 @@ var GuaGame = function(fps) {
   window.fps = 30;
   var runloop = function() {
     // events
-    log(window.fps)
     var actions = Object.keys(g.actions);
     for (var i = 0; i < actions.length; i++) {
       var key = actions[i];
